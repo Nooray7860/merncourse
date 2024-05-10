b49d70dce16f7c4f04288ff484976081ed354a1b
afaqzz <afaq.ahmad411@icloud.com>
2024-05-10 17:30:48 +0500

    just to check

--- main.js
+++ main.js
@@ -4,31 +4,29 @@ function pallindrome() {
 
 pallindrome();
 
-function cal(){
+function cal() {
+  console.log("cal");
+}
 
-    console.log("cal");
+cal();
+function calculate(num1, num2, operation) {
+  let result;
+  switch (operation) {
+    case "add":
+      result = num1 + num2;
+      break;
+    case "subtract":
+      result = num1 - num2;
+      break;
+    case "multiply":
+      result = num1 * num2;
+      break;
+    case "division":
+      result = num1 / num2;
+      break;
+    default:
+      result = "Invalid operation";
   }
-  
-  cal();
-  function calculate(num1,num2,operation){
-    let result;
-    switch(operation){
-        case 'add':
-            result = num1 + num2;
-            break;
-        case 'subtract':
-            result = num1 - num2;
-            break;  
-        case 'multiply':
-            result = num1 * num2;
-            break;
-        case 'division':
-           result = num1 / num2 ;
-           break;
-        default;
-        result = "Invalid operation";
-
-    }
-    result;
-  }
-  console.log(calculate(10,20,'add'));
+  return result;
+}
+console.log(calculate(10, 20, "subtract"));
