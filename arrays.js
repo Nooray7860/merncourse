ac036d6e3430a4ed4464cd97b3a0a2496fee3c0f
afaqzz <afaq.ahmad411@icloud.com>
2024-05-14 20:41:35 +0500

    arrays and objects

--- arrays.js
+++ arrays.js
@@ -1,7 +1,15 @@
 // Storing and accessing values in arrays
 // Methods: push, pop, shift, unshift, indexOf, includes
+// slice, splice, reverse, concat, sort, join
+// bete muneshka this is your home task
+
 // De-structuring
 
-const array = [1, "one", true, null, undefined];
+const array1 = [1, "one", true, null, undefined];
 
-console.log(array);
+const array2 = array1;
+
+array2[0] = "new check";
+
+console.log("array1", array1);
+console.log("array2", array2);
---
+++ objects.js
@@ -0,0 +1,15 @@
+const person = {
+  name: "shoffi",
+  age: 23,
+  dob: "oct 28 2000",
+};
+
+person["gender"] = "F";
+person.religion = "islam";
+person.nationality = "pakistani";
+
+// console.log(person);
+
+// Methods: keys, values, entries
+
+console.log(Object.keys(person));
